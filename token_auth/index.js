ba7e302cc52ca5eb8e741cd144cb49c7a757c115
andriibosyk <andriybosik2002@ukr.net>
2022-12-04 21:01:02 +0200

    Integrate Auth0

--- token_auth/index.js
+++ token_auth/index.js
@@ -1,145 +1,57 @@
-const jwt = require('jsonwebtoken');
 const express = require('express');
-const onFinished = require('on-finished');
 const bodyParser = require('body-parser');
 const path = require('path');
 const port = 3000;
-const fs = require('fs');
+const {login, register, refresh} = require("./auth");
+const {auth} = require("express-oauth2-jwt-bearer");
 
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-const SESSION_KEY = 'Authorization';
-
-const getJwt = () => {
-    return jwt.sign(
-        {},
-        "secretJwtKey",
-        {
-            expiresIn: "30m"
-        }
-    );
-}
-
-class Session {
-    #sessions = {}
-
-    constructor() {
-        try {
-            this.#sessions = fs.readFileSync('./sessions.json', 'utf8');
-            this.#sessions = JSON.parse(this.#sessions.trim());
-
-            console.log(this.#sessions);
-        } catch (e) {
-            this.#sessions = {};
-        }
-    }
-
-    #storeSessions() {
-        fs.writeFileSync('./sessions.json', JSON.stringify(this.#sessions), 'utf-8');
-    }
-
-    set(key, value) {
-        if (!value) {
-            value = {};
-        }
-        this.#sessions[key] = value;
-        this.#storeSessions();
-    }
-
-    get(key) {
-        return this.#sessions[key];
-    }
-
-    init(res) {
-        const sessionId = getJwt();
-        this.set(sessionId);
-
-        return sessionId;
-    }
-
-    destroy(req, res) {
-        const sessionId = req.sessionId;
-        delete this.#sessions[sessionId];
-        this.#storeSessions();
-    }
-}
-
-const sessions = new Session();
-
-app.use((req, res, next) => {
-    let currentSession = {};
-    let sessionId = req.get(SESSION_KEY);
-
-    if (sessionId) {
-        currentSession = sessions.get(sessionId);
-        if (!currentSession) {
-            currentSession = {};
-            sessionId = sessions.init(res);
-        }
-    } else {
-        sessionId = sessions.init(res);
-    }
-
-    req.session = currentSession;
-    req.sessionId = sessionId;
-
-    onFinished(req, () => {
-        const currentSession = req.session;
-        const sessionId = req.sessionId;
-        sessions.set(sessionId, currentSession);
-    });
-
-    next();
+const checkJwt = auth({
+    audience: "https://kpi.eu.auth0.com/api/v2/",
+    issuerBaseURL: "https://kpi.eu.auth0.com/"
 });
 
 app.get('/', (req, res) => {
-    if (req.session.username) {
-        return res.json({
-            username: req.session.username,
-            logout: 'http://localhost:3000/logout'
-        })
-    }
     res.sendFile(path.join(__dirname + '/index.html'));
-})
-
-app.get('/logout', (req, res) => {
-    sessions.destroy(req, res);
-    res.redirect('/');
 });
 
-const users = [
-    {
-        login: 'Login',
-        password: 'Password',
-        username: 'Username',
-    },
-    {
-        login: 'Login1',
-        password: 'Password1',
-        username: 'Username1',
+app.post('/api/login', async (req, res) => {
+    const {username, password} = req.body;
+    const access = await login(username, password);
+
+    if (access) {
+        res.json(access);
     }
-]
-
-app.post('/api/login', (req, res) => {
-    const {login, password} = req.body;
-
-    const user = users.find((user) => {
-        if (user.login == login && user.password == password) {
-            return true;
-        }
-        return false
-    });
-
-    if (user) {
-        req.session.username = user.username;
-        req.session.login = user.login;
-
-        res.json({token: req.sessionId});
-    }
-
     res.status(401).send();
+});
+
+app.post('/api/register', async (req, res) => {
+    const {username, password} = req.body;
+    const user = await register(username, password);
+    if (user) {
+        return res.json(user);
+    }
+    return res.status(500).send();
+});
+
+app.get("/api/private", checkJwt, async (req, res) => {
+    return res.json({
+        message: "Message for authorized members!"
+    });
+});
+
+app.post("/api/token", async (req, res) => {
+    const {refreshToken} = req.body;
+    const result = await refresh(refreshToken);
+    if (result) {
+        return res.json({
+            accessToken: result.access_token
+        });
+    }
+    return res.status(500).send();
 });
 
 app.listen(port, () => {
